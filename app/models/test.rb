e75205cc2ea60ffccab486e9c5c5c8e9b7d59b9a
Lana-243 <svetaisupova@gmail.com>
2022-01-14 20:58:30 +0300

    changed syntax

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.titles(category)
-    Test.select("title").joins("JOIN categories ON categories.category_id = tests.category_id AND categories.category_id = category").order(:title DESC)
+    Test.select("title").joins("JOIN categories ON categories.category_id = tests.category_id AND categories.category_id = " + category_to_s).order(title: :desc)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Result.where("user_id = self.id").joins("JOIN tests ON tests.test_id = results.test_id AND tests.level = level")
+    Result.where("user_id = self.id").joins("JOIN tests ON tests.test_id = results.test_id AND tests.level = " + level.to_s)
   end
 end
