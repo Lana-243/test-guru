d24654b08c2af7d80b0339a4445e650c5d8f501b
Lana-243 <svetaisupova@gmail.com>
2022-01-14 19:14:58 +0300

    added result table, seed data, User; Test methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.titles(category)
+    Test.select("title").joins("JOIN categories ON categories.category_id = tests.category_id AND categories.category_id = category").order(:title DESC)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    Result.where("user_id = self.id").joins("JOIN tests ON tests.test_id = results.test_id AND tests.level = level")
+  end
 end
