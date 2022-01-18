5146b841ec49709dd42a311366769f8f941b2745
Lana-243 <svetaisupova@gmail.com>
2022-01-18 13:25:14 +0300

    changed WHERE in test and user

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
-  def self.titles_by_category(category)
-    Test.joins("JOIN categories ON categories.category_id = tests.category_id").where(categories.title: category).pluck(:tests.title).order(title: :desc)
+  def self.titles_by_category(category_title)
+    joins("JOIN categories ON categories.category_id = tests.category_id")
+    .where(categories: { title: category_title })
+    .pluck(tests: { title: })
+    .order(tests: { title: :desc })
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Result.joins("JOIN tests ON tests.test_id = results.test_id").where(results.user_id: self.id, tests.level: level).pluck(:tests.title)
+    Result.joins("JOIN tests ON tests.test_id = results.test_id")
+    .where(results: { user_id: self.id })
+    .where(tests: { level: level })
+    .pluck(tests: { title: })
   end
 end
