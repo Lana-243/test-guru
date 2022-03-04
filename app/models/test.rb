bd82ebfd2309ecc20ffa2c5bd5cdfbfbf970d6eb
Lana-243 <svetaisupova@gmail.com>
2022-03-04 13:24:12 +0300

    addition

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-
   belongs_to :author, class_name: "User", foreign_key: :author_id
 
   has_many :questions, dependent: :destroy
-  
   has_many :results, dependent: :destroy
-  
   has_many :users, through: :results, dependent: :destroy
   
   def self.titles_by_category(category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   has_many :tests, foreign_key: "author_id", dependent: :destroy
-
   has_many :results, dependent: :destroy
-  
   has_many :tests, through: :results, dependent: :destroy
+
   def tests_by_level(level)
     Result
     .joins(:tests)
     .where(results: { user_id: self.id })
     .where(tests: { level: level })
-    .pluck(:title)
   end
 end
