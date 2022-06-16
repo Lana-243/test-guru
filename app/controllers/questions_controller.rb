3b335a419bf7bf9cd0095f5301701020e5466085
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-06-16 10:47:56 +0300

    added controller methods to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,51 @@
 class QuestionsController < ApplicationController
-  before_action :set_question
+  before_action :set_test
+  before_action :set_question, only: %i[show destroy]
 
-  def index
-    respond_to do |format|
-      format.html { render plain: "All questions" }
-      format.text { render text: { questions: Questions.all } }
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def new
+    @question = @test.questions.build
+  end
+
+  def create
+    @question = @test.questions.build question_params
+    if @question.save
+      redirect_to test_questions_path(@test)
+    else
+      render :new
     end
   end
 
+  def index
+    questions = @test.questions.map { |question| question.title.to_s }
+    render plain: questions.join
+  end
+
   def show
-    render test_question
+    render plain: @question.title
   end
 
   def destroy
     @question.destroy
-
-    redirect_to test_questions, notice: "Question has been deleted"
+    redirect_to test_questions_path(@test), notice: 'Question has been deleted'
   end
 
   private
 
-  def set_question
-    @question = Question.find(params[:id])
+  def question_params
+    params.require(:question).permit(:title)
   end
 
   def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_question
+    @question = Test.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 end
